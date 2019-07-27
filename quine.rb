f955baa48a8fc2d631e867d022a27bee52adb272
Seiei Miyagi <hanachin@gmail.com>
2019-07-27 16:28:10 +0900

    PHP! PHP!

--- quine.rb
+++ quine.rb
@@ -1,1 +1,1 @@
-eval(S="require 'json';puts 'print '+('print '+('console.log('+('eval(S='+S.inspect+')').inspect.to_json+')').inspect).inspect")
+eval(S="require 'json';puts %(<?php\necho ) + ('print '+('print '+('console.log('+('eval(S='+S.inspect+')').inspect.to_json+')').inspect).inspect).inspect + ?;")
