e9712d4b2484c4d28c14a50c4230076b4c7f85e7
Seiei Miyagi <hanachin@gmail.com>
2019-07-31 23:32:47 +0900

    WIP

--- snake_generator.rb
+++ snake_generator.rb
@@ -11,6 +11,7 @@ using Module.new {
 }
 
 im = -> { Array.new(H) { Array.new(W) { ' ' } } }
+m = im.()
 
 ts = []
 fs = []
@@ -18,9 +19,9 @@ fs = []
 define_method(:new_body_from){|t|
   nb = t.b.dup
   h = nb.first
-  tail = nb.pop
+  e = nb.pop
   nh =
-    case t.dir
+    case t.d
     when "\e[A"
       [(h.y - 1) % H, h.x]
     when "\e[B"
@@ -33,19 +34,15 @@ define_method(:new_body_from){|t|
       h
     end
   nb.unshift(nh)
-
-  fs.delete([nh.y, nh.x]) && nb.push(tail)
+  fs.delete([nh.y, nh.x]) && nb.push(e)
   nb
 }
 
 
 rt = Thread.start {
-  m = im.()
-  Thread.current.define_singleton_method(:m) { m }
   Thread.current.define_singleton_method(:bp) {
     loop {
-      y = rand(0...H)
-      x = rand(0...W)
+      x,y = rand(W),rand(H)
       break [y, x] if m[y][x] == ' '
     }
   }
@@ -55,57 +52,44 @@ rt = Thread.start {
     ts.each { |t|
       t.b = new_body_from(t)
       t.b.each { |y,x|
-        nm[y][x] = t.player
+        nm[y][x] = t.p
       }
     }
     m = nm
 
     ts.each { |t|
       ts.each { |t2|
-        next if t == t2
-        t2.b.include?(t.b.first) && t.dead
+        t != t2 &&  t2.b.include?(t.b.first) && t.dead
       }
     }
 
-    fs.count < 3 && fs << Thread.current.bp
+    fs.size < 3 && fs << Thread.current.bp
 
-    fs.each { |f| m[f.y][f.x] = '@' }
+    fs.each { m[@1][@2] = '@' }
     sleep(0.1)
   }
 }
 
-using Module.new {
-  refine(TCPSocket) {
-    define_method(:render) {
-      rt.m.each_with_index { |r,y|
-        print("\x1b[#{y+1};1H\x1b[K",*r)
-      }
-    }
-  }
-}
-
-player = ?A
+p = ?A
 
 gs = TCPServer.open(1234)
 
 loop {
   ts << Thread.start(gs.accept) { |s|
-    new_player = player
-    player = player.next
-    Thread.current.define_singleton_method(:player) { new_player }
-    Thread.current.define_singleton_method(:s) { s }
-
-    dir = ""
-    Thread.current.define_singleton_method(:dir) { dir }
-
-    live = true
-    Thread.current.define_singleton_method(:dead) { live = false }
-
+    np = p
+    p = p.next
+    d = ""
+    l = true
     b = [rt.bp]
-    Thread.current.define_singleton_method(:b=) {|nb| b = nb }
-    Thread.current.define_singleton_method(:b) { b }
 
-    s.print([255, 253, 34, 255, 250, 34, 1, 0, 255, 240, 255, 251, 1].pack('c*'))
+    fn=Thread.current.:define_singleton_method
+    fn.(:p) { np }
+    fn.(:d) { d }
+    fn.(:dead) { l= false }
+    fn.(:b=) {|nb| b = nb }
+    fn.(:b) { b }
+
+    s.print([255, 253, 34, 255, 250, 34, 1, 0, 255, 240, 255, 251, 1].pack('c*'),"\x1b[2J")
 
     begin
       sleep(0.1)
@@ -114,18 +98,18 @@ loop {
       retry
     end
 
-    s.print("\x1b[2J")
-
     loop {
       begin
-        dir = s.read_nonblock(3)
+        d = s.read_nonblock(3)
       rescue IO::EAGAINWaitReadable
         nil
-      rescue EOFError
+      rescue
         break
       end
-      s.render
-      !live && break
+      m.each_with_index { |r,y|
+        s.print("\x1b[#{y+1};1H\x1b[K",*r)
+      }
+      !l && break
     } rescue nil
     s.close
     ts.delete(Thread.current)
