2e84456b8923325d8e73d2acdd0d9caa64ea3535
Seiei Miyagi <hanachin@gmail.com>
2019-08-03 15:13:05 +0900

    WIP

--- snake_generator.rb
+++ snake_generator.rb
@@ -1,100 +1,96 @@
-C=File.read(__FILE__).split('AA'+'BB').last.gsub(/ +/, ' ').gsub(/^ +/, '')
-puts C
+C=File.read(__FILE__).split('AA'+'BB').last.gsub(/ +/, ' ').gsub(/^ +/, '').lines.map(&:chomp).reject {|s|
+  s.start_with?('#')||s.empty?
+}.join
+puts C.size
 # eval C
 # exit
-
-# AABB
-require'socket'
-
-H,W=24,80
-
 # def render
 #   formatted = C wo asciiaato ni kuuhaku ireru
 #   puts "eval C=%w(#{formatted}).join"
 # end
 
-using Module.new{refine(Array){define_method(:y){first};define_method(:x){last}}};
+# AABB
+require'socket';
+
+H,W,T,A=24,80,Thread,Array;
+using Module.new{refine(A){define_method(:y){first};define_method(:x){last}}};
 m,ts,fs=nil,[],[];
 dm,bp,rt=
       {"\e[A"=>->y,x{[(y-1)%H,x]},"\e[B"=>->y,x{[(y+1)%H,x]},"\e[C"=>->y,x{[y,(x+1)%W]},"\e[D"=>->y,x{[y,(x-1)%W]}},
 ->{loop{x,y=rand(W),rand(H);return[y,x]if m[y][x]==' '}},
-Thread.start{
+T.start{
   loop{
-    nm=Array.new(H){Array.new(W){' '}}
+    nm=A.new(H){A.new(W){' '}};
     ts.each{|t|
-      t.n
+      t.n;
       t.b.each{|y,x|nm[y][x]=t.p}
-    }
-    m=nm
-    ts.combination(2){@2.b.include?(@1.b.first)&&@1.e}
-    fs.size<10&&fs<<bp.()
-    fs.each{m[@1][@2]='@'}
-    # ci = 0
-    # m2 = Array.new(H*8) { Array.new(W*8) { ' ' } }
-    # m.each_with_index { |r, y|
-    #   r.each_with_index { |c, x|
-    #     if c == '@'
-    #       8.times { |yi|
-    #         ci+=8
-    #         m2[y*8+yi][x..x+8] = C[ci-8..ci]
-    #       }
-    #     elsif c == ' '
-    #       8.times { |yi|
-    #         m2[y*8+yi][x...x+8] = ' ' * 8
-    #       }
-    #     else
-    #       8.times { |yi|
-    #         m2[y*8+yi][x...x+8] = (c * 6).inspect
-    #       }
-    #     end
-    #   }
-    # }
+    };
+    m=nm;
+    ts.combination(2){@2.b.include?(@1.b.first)&&@1.e};
+    fs.size<30&&fs<<bp.();
+    fs.each{m[@1][@2]='@'};
 
-    # m.each_with_index { |r, y|
-    #   s.print("\x1b[#{y+1};1H\x1b[K",*r)
-    # }
-    # m2.each_with_index { |r,y|
-    #   $stdout.print("\x1b[#{y+1};1H\x1b[K",*r)
-    # }
+    # koko
+    ci=0
+    m2=Array.new(H*N){Array.new(W*N){' '}}
+    m.each_with_index{|r,y|
+      r.each_with_index{|c,x|
+        if c=='@';
+          N.times{|yi|
+            ci+=N;
+            m2[y*N+yi][x..x+N]=C[ci-N..ci]
+          }
+        elsif c==' ';
+          N.times{|yi|m2[y*N+yi][x...x+N]=' '*N};
+        else;
+          N.times{|yi|m2[y*N+yi][x...x+N]=(c*N)};
+        end
+      }
+    }
+    # "\x1b[48;5;%dm%03d\x1b[0m"%[@1,@1]
+    m2.each_with_index{|r,y|
+      # $stdout.print("\x1b[#{y+1};1H\x1b[K",*r)
+    }
+    # made
 
     sleep(0.1)
   }
-}
+};
 
-p=?A
+p=?A;
 
-gs=TCPServer.open(1234)
+gs=TCPServer.open(1234);
 
 loop{
-  ts<<Thread.start(gs.accept){|s|
-    q,d,l,b,p=p,"",true,[bp.()],p.next
-    fn=Thread.current.:define_singleton_method
-    fn.(:p){q}
-    fn.(:d){d}
-    fn.(:e){l=false}
-    fn.(:b){b}
-    fn.(:n){
-      nb=b.dup
-      h=nb.first
-      e=nb.pop
-      nh=dm[d]&.(*h)||h
-      nb.unshift(nh)
-      fs.delete(nh)&&nb.push(e)
-      b=nb
-    }
-    s.print([255,253,34,255,250,34,1,0,255,240,255,251,1].pack('c*'),"\x1b[2J\x1b[?12l")
+  ts<<T.start(gs.accept){|s|
+    q,d,l,b,p=p,"",true,[bp.()],p.next;
+    define_method(q*N){|*|};
+    f=T.current.:define_singleton_method;
+    f.(:p){q};
+    f.(:d){d};
+    f.(:e){l=false};
+    f.(:b){b};
+    f.(:n){
+      nb=b.dup;
+      h=nb.first;
+      e=nb.pop;
+      nh=dm[d]&.(*h)||h;
+      nb.unshift(nh);
+      fs.delete(nh)&&nb.push(e);
+      b=nb;
+    };
+    s.print([255,253,34,255,250,34,1,0,255,240,255,251,1].pack('c*'),"\x1b[2J\x1b[?12l");
     loop{
-      begin
-        d=s.read_nonblock(3)
-      rescue IO::EAGAINWaitReadable
-      rescue
-        break
-      end
-
-      m.each_with_index{|r,y|s.print("\x1b[#{y+1};1H\x1b[K",*r)}
+      begin;
+        d=s.read_nonblock(3);
+      rescue IO::EAGAINWaitReadable;
+      rescue;
+        break;
+      end;
+      m.each_with_index{|r,y|s.print("\x1b[#{y+1};1H\x1b[K",*r)};
       l||break
-    }rescue 1
-    s.close
-    ts.delete(Thread.current)
+    }rescue 1;
+    s.close;
+    ts.delete(T.current)
   }
 }
