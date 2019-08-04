4471175d3d4dfc0c80d410ee77ae1cbbf9eedfe7
Seiei Miyagi <hanachin@gmail.com>
2019-08-05 04:13:35 +0900

    WIP

--- foo.rb
+++ foo.rb
@@ -2,13 +2,13 @@ eval(C=%w(require'socket';H,W,T,A=24,80,Thread,Array;using(Module.new{refine(A){
 define_method(:y){first};define_methoxxxxxxlast}}});m,ts,fs=nil,[],[];dm,bp,rt={
 "\e[A"=>->y,x{[(y-1)%H,x]},"\e[B"=>-      xxxxxxxxxx{[(y+1)%H,x]},"\e[C"=>->y,x{
 ,"\e[D"=>->y,x{[y,(x-1)%W]}xxxxxxxx          xx,y=rand(W),rand(H);unless(m[xxxxx
-th?("\x1b"));return[y,x];end}}xxx    ____     xx\rt{C="eval(C=%w(#{C}).join)";xx
-){A.new(W){'x'}};i=0;nm.each_wxx    _/. . \    xxxxx~|r,y|r.each_with_index{|c,n
-]||'#';i+=1}};tts=ts.select{|txx    \___  /     xxx//to?(:n)};tts.aaaaaaaaaaaaaa
-nm[y][x]="\x1b[48;5;%dm%sxxxxxx   >~~\=/ /       xxx\x1b[0m"%xxt.p,nm[y][x]]}};m
+th?("\x1b"));return[y,x];end}}xxx     ____    xx\rt{C="eval(C=%w(#{C}).join)";xx
+){A.new(W){'x'}};i=0;nm.each_wxx    _/. . L    xxxxx~|r,y|r.each_with_index{|c,n
+]||'#';i+=1}};tts=ts.select{|txx    L___  /     xxx//to?(:n)};tts.aaaaaaaaaaaaaa
+nm[y][x]="\x1b[48;5;%dm%sxxxxxx   >~~L=/ /       xxx\x1b[0m"%xxt.p,nm[y][x]]}};m
 include?(@1.b.first)&&@xxxxxxxx       | /        xx;@1.b.include?(@2.b.first)&&@
 );fs.each{m[@1][@2]="\x1bxxxxxx       | |__/|    xx[48;5;0m%s\x1b[0m"%m[@1][@2]}
-index{|r,y|"\x1b[#{y+1};1H\xxxxx       \_____/   xx|"+r.join}.join);sleep(0.1)}}
+index{|r,y|"\x1b[#{y+1};1H\xxxxx       L____/    xx|"+r.join}.join);sleep(0.1)}}
 en(1234);begin;loop{ts<<T.startxx               xx(gs.accept){|s|q,d,l,b=(p=p.xx
 ;f=T.current.:define_singleton_mxxx           xxxxxx:p){q};f.(:d){d};f.(:e){xxxx
 {b};f.(:n){nb=b.dup;xxxx.first;e=nb.pop;nh=dm[d]&xxxxxxxxx.(*h)||h;nb.unshift(nh
